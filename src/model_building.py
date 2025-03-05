f7e5a1958de49f26c7edcaf6c145a1d8c9983882
Ruz <ruzsan.bhadha@gmail.com>
2025-03-05 10:55:06 +0530

    Params configured for the pipeline

--- src/model_building.py
+++ src/model_building.py
@@ -28,7 +28,7 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-"""def load_params(params_path: str) -> dict:
+def load_params(params_path: str) -> dict:
     #Load parameters from a YAML file.
     try:
         with open(params_path, 'r') as file:
@@ -44,7 +44,7 @@ logger.addHandler(file_handler)
     except Exception as e:
         logger.error('Unexpected error: %s', e)
         raise
-"""
+
 
 def load_data(file_path: str) -> pd.DataFrame:
     """
@@ -119,8 +119,8 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        #params = load_params('params.yaml')['model_building']
-        params = {'n_estimators':25, 'random_state':2}
+        params = load_params('params.yaml')['model_building']
+        #params = {'n_estimators':25, 'random_state':2}
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
